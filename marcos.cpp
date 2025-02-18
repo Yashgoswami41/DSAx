8857813785d18c00b79106d2a11183cc389383b8
Yash Goswami <145782019+Yashgoswami41@users.noreply.github.com>
2025-02-18 20:49:31 +0530

    Create marcos.cpp

---
+++ marcos.cpp
@@ -0,0 +1,15 @@
+#include <iostream> 
+using namespace std;
+
+#define PI 3.14  //<--macro 
+
+int main() {
+
+    int r = 76;
+
+    double area = PI * r * r;
+
+    cout << "Area is " << area << endl;
+
+    return 0;
+}
